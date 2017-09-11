031c2cfed34d54775483cb7696d3c8134d86d79f
twskj <>
2017-09-11 00:21:06 -0500

    add sender.go

---
+++ helloWorld/sender.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
+}
+
+func main() {
+
+	connStr := "amqp://guest:guest@localhost:5672/"
+
+	conn := connect(connStr)
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"helloQueue", // name
+		false,        // durable
+		false,        // delete when unused
+		false,        // exclusive
+		false,        // no-wait
+		nil,          // arguments
+	)
+
+	failOnError(err, "Failed to declare a queue")
+
+	body := "hello"
+
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		},
+	)
+
+	failOnError(err, "Failed to publish a message")
+
+}
+
+func connect(connStr string) *amqp.Connection {
+
+	conn, err := amqp.Dial(connStr)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	return conn
+}
