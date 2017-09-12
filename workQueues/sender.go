059adc0489a6d17942c2c45da8c35ea216ac76de
twskj <>
2017-09-11 19:38:37 -0500

    add worker mode

---
+++ workQueues/sender.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
+}
+
+func main() {
+
+	connStr := "amqp://guest:guest@localhost:5672/"
+
+	conn := connect(connStr)
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"workQueues", // name
+		false,        // durable
+		false,        // delete when unused
+		false,        // exclusive
+		false,        // no-wait
+		nil,          // arguments
+	)
+
+	failOnError(err, "Failed to declare a queue")
+
+	enter := make(chan string)
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool)
+
+	go func() {
+
+		src := rand.NewSource(time.Now().UnixNano())
+		ticker := time.NewTicker(time.Millisecond * 1000)
+		for _ = range ticker.C {
+			tmp := src.Int63() % 5000
+			enter <- strconv.FormatInt(tmp, 10)
+		}
+	}()
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+
+		for {
+			select {
+			case line := <-enter:
+				body := []byte(line)
+				err = ch.Publish(
+					"",     // exchange
+					q.Name, // routing key
+					false,  // mandatory
+					false,  // immediate
+					amqp.Publishing{
+						ContentType: "text/plain",
+						Body:        []byte(body),
+					},
+				)
+				log.Printf(" [x] Sent workload %s ms", body)
+				failOnError(err, "Failed to publish a message")
+
+			case _ = <-sigs:
+				done <- true
+			}
+		}
+	}()
+
+	<-done
+	log.Printf("bye")
+}
+
+func connect(connStr string) *amqp.Connection {
+
+	conn, err := amqp.Dial(connStr)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	return conn
+}
---
+++ workQueues/worker.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"time"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
+}
+
+func connect(connStr string) *amqp.Connection {
+
+	conn, err := amqp.Dial(connStr)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	return conn
+}
+
+func main() {
+
+	connStr := "amqp://guest:guest@localhost:5672/"
+	conn := connect(connStr)
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"workQueues", // name
+		false,        // durable
+		false,        // delete when usused
+		false,        // exclusive
+		false,        // no-wait
+		nil,          // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+			num, err := strconv.Atoi(string(d.Body))
+			if err != nil {
+				log.Printf("Invalid Message: %v", num)
+			}
+			time.Sleep(time.Millisecond * time.Duration(num))
+			log.Printf("Done")
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
